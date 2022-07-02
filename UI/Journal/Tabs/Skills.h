57d825b044f06e8f074cb90f5d0760d0c110bba1
RinWate <denisegorov75@gmail.com>
2022-07-02 18:28:57 +0300

    New files

---
+++ UI/Journal/Tabs/Skills.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Skills.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNING_API USkills : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Utility/RUtility.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RUtility.h"
+
+void RUtility::PrintMessage(FString Message)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, Message);
+	}
+}
